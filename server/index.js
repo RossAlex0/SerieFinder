9d272925d998c59ae706335aef6bf556a15d6fc9
Rossignol Alex <alexnw33910@gmail.com>
2024-07-29 12:50:03 +0200

    ⭐ Initial commit

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ const app = require("./app/config");
 const port = process.env.APP_PORT;
 
 // Start the server and listen on the specified port
+const sayWelcome = ((req, res) => {
+  res.send("Welcome to SerieFinder !!");
+})
+app.get("/", sayWelcome)
+
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);

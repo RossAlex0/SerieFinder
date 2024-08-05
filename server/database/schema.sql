edccbc0c9e23a13d612ef0c3f24dcb469ad71810
Rossignol Alex <alexnw33910@gmail.com>
2024-08-05 16:32:09 +0200

    Modification .env & crétation de table program et category

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,21 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE Table category (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE program ( 
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(250) not null,
+  synopsis text not null,
+  poster VARCHAR(250),
+  country VARCHAR(250) not null,
+  year INT NOT NULL,
+  category_id INT not null,
+  Foreign Key (category_id) REFERENCES category(id)
+);
+
+
